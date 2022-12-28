cec9724ca6906688a55b01f71f97af720bf85d4f
M Naveed <chnaveed78611@gmail.com>
2022-12-28 12:22:12 +0500

    Final Integration file done

---
+++ my-app/constants/index.js
@@ -0,0 +1,67 @@
+export const WHITELIST_CONTRACT_ADDRESS =
+  "0xACaB74241dc229C1EfE5dbc867c62f2a6641656c";
+export const abi = [
+  {
+    inputs: [
+      {
+        internalType: "uint8",
+        name: "_maxwhitelistaddress",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "addAddressToWhitelist",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "maxwhitelistaddress",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "numwhitelistaddress",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "whitelistaddress",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
